d56c7a0eea93e1d7910701787ba1ca797dc20105
Salomon Orrego Martinez <sorregom@psl.com.co>
2019-12-06 18:00:30 -0500

    adding some actions to path_example

--- src/controllers/path_example/path-example.controller.js
+++ src/controllers/path_example/path-example.controller.js
@@ -1,11 +1,12 @@
 function postPathExample(req, res) {
-  const { val } = req.params;
+  let { val } = req.params;
 
   if (val.length < 3) {
     res
       .status(400)
       .send('Bad Request, value cannot be parsed');
   } else {
+    val = val.toUpperCase();
     res
       .status(201)
       .json({ val });
